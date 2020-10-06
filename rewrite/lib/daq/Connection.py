7babc3613b451fe834cc018b053c311f7e004406
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-10-06 15:33:53 +0200

    Can read data with the new reduced architecture
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- rewrite/lib/daq/Connection.py
+++ rewrite/lib/daq/Connection.py
@@ -1,14 +1,113 @@
+import logging
+import serial
+from .getDevice import get_Device
+from time import sleep
 
-
-
-class BaseDAQConnection(metaclass=object):
+class DAQConnection(object):
     
     """
-    Base DAQ Connection class.
+    DAQ Connection class.
 
     Raises SystemError if serial connection cannot be established.
 
     :param logger: logger object
     :type logger: logging.Logger
+    :param in_queue: input queue
+    :param out_queue: output queue
     :raises: SystemError
     """
+
+    def __init__(self, in_queue, out_queue, logger=None):
+        if logger is None: 
+            logger = logging.getLogger()
+        self.logger = logger
+        self.running = 1
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+        try:
+            self.serial_port = self.get_serial_port()
+        except serial.SerialException as e:
+            self.logger.fatal(f"SerialException! Error: {e.message}")
+            raise SystemError(e)
+
+    def get_serial_port(self):
+        """
+        Check out which device (/dev/tty) is used for DAQ communication.
+
+        Raises OSError if binary 'which_tty_daq' cannot be found.
+
+        :returns: serial.Serial -- serial connection port
+        :raises: OSError
+        """
+
+        connected = False
+        serial_port = None
+
+        while not connected: 
+            dev = "/dev/" + get_Device()
+
+            self.logger.info(f"DAQ Card found at {dev}")
+            self.logger.info("Trying to connect")
+
+            try:
+                serial_port = serial.Serial(port=dev, baudrate=115200,
+                                            bytesize=8, parity='N', stopbits=1,
+                                            timeout=0.5, xonxoff=True)
+                connected = True
+            except serial.SerialException as e:
+                self.logger.error(e)
+                self.logger.error("Retrying in 5 seconds")
+                sleep(5)
+        self.logger.info("Successfully connected to DAQ card")
+        return serial_port
+    
+    def read(self):
+        """
+        Gets Data from the DAQ card. Read it from the provided queue.
+
+        :returns: None
+        """
+
+        min_sleep_time = 0.01 #seconds
+        max_sleep_time = 0.2  #seconds
+        sleep_time = min_sleep_time  # seconds
+
+        while self.running:
+            try:
+                if self.serial_port.inWaiting():
+                    while self.serial_port.inWaiting():
+                        self.out_queue.put(self.serial_port.readline().strip())
+                        sleep_time = max(sleep_time / 2, min_sleep_time)
+                else:
+                    sleep_time = min(1.5 * sleep_time, max_sleep_time)
+                sleep(sleep_time)
+            except (IOError, OSError):
+                self.logger.error("IOError")
+                self.serial_port.close()
+                self.serial_port = self.get_serial_port()
+        
+    def write(self):
+        """
+        Writes messages from the in queue to the DAQ card
+
+        :returns: None
+        """
+
+        while self.running:
+            try:
+                while self.in_queue.qsize():
+                    try:
+                        out_str = str(self.in_queue.get(0)) + str("\r")
+                        self.serial_port.write(out_str.encode("ascii"))
+                    except (queue.Empty, serial.SerialTimeoutException):
+                        pass
+            except NotImplementedError:
+                self.logger.debug("Running macOS version of muonic")
+                while True:
+                    try:
+                        out_str = str(self.in_queue.get(timeout=0.01)) + str("\r")
+                        self.serial_port.write(out_str.encode("ascii"))
+                    except (queue.Empty, serial.SerialTimeoutException):
+                        pass
+            sleep(0.1)
---
+++ rewrite/lib/daq/Provider.py
@@ -0,0 +1,93 @@
+import logging
+import re
+import multiprocessing as mp
+import queue
+from .Connection import DAQConnection
+
+from .Exceptions import DAQIOError, DAQMissingDependencyError
+
+class DAQProvider(object):
+    """
+    Class providing the public API and helpers for the communication with the DAQ card
+    """
+    
+    LINE_PATTERN = re.compile("^[a-zA-Z0-9+-.,:()=$/#?!%_@*|~' ]*[\n\r]*$")
+    
+    def __init__(self, logger=None):
+        if logger is None:
+            logger = logging.getLogger()
+        self.logger = logger
+        self.out_queue = mp.Queue()
+        self.in_queue = mp.Queue()
+
+        self.daq = DAQConnection(self.in_queue, self.out_queue, self.logger)
+
+        # Set up the thread to do asynchronous I/O. More can be made if
+        # necessary. Set daemon flag so that the threads finish when the main
+        # app finishes
+        self.read_thread = mp.Process(target=self.daq.read, name="pREADER")
+        self.read_thread.daemon = True
+        self.read_thread.start()
+
+        self.write_thread = mp.Process(target=self.daq.write, name="pWRITER")
+        self.write_thread.daemon = True
+        self.write_thread.start()
+
+    def get(self, *args):
+        """
+        Get data from the DAQ card. 
+
+        Raises DAQIOError if the queue is empty.
+
+        :param args: queue arguments
+        :type args: list
+        :returns: str or None -- next item from the queue
+        :raises: DAQIOError
+        """
+        try:
+            line = self.out_queue.get(*args)
+        except queue.Empty:
+            raise DAQIOError("Queue is empty")
+        return self.validate_line(line)
+
+    def put(self, *args):
+        """
+        Senf data to the DAQ card.
+
+        :param args: queue arguments
+        :type args: list
+        :returns: None
+        """
+        self.in_queue.put(*args)
+
+    def data_available(self):
+        """
+        Tests if data is available from the DAQ card.
+        
+        :returns: int or bool
+        """
+        try:
+            size = self.out_queue.qsize()
+        except NotImplementedError:
+            self.logger.debug("Running macOS version of muonic")
+            size = not self.out_queue.empty()
+        return size
+
+
+
+
+
+    def validate_line(self, line):
+        """
+        Validate line againt regex pattern. 
+        Returns None if the provided line is invalid or the line if it is valid.
+
+        :param line: line to validate
+        :type line: str
+        :returns: str or None
+        """
+        if self.LINE_PATTERN.match(line.decode("ascii")) is None:
+            line = line.rstrip('\r\n')
+            self.logger.warning(f"Got invalid data from the DAQ card: {line}")
+            return None
+        return line

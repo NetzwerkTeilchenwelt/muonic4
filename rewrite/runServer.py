7b1dce8509a456057862543af0cfcf2f7ed3515b
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-11-03 13:40:33 +0100

    added Docstrings to runServer.py
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- rewrite/runServer.py
+++ rewrite/runServer.py
@@ -3,10 +3,16 @@ from lib.daq.DAQServer import DAQServer
 
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
+    """
+    Adapter Class for xmlrpc
+    """
     rpc_paths = ('/RPC2',)
 
 
 def run():
+    """
+    Starts an instance of the DAQ server with xmlrpc enabled and then enters an infinite loop
+    """
     with SimpleXMLRPCServer(('localhost', 5556), requestHandler=RequestHandler, allow_none=True) as server:
         server.register_introspection_functions()
         server.register_instance(DAQServer())

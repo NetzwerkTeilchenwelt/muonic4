34f8011872b40c42b43371518a5cb810c165a14b
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-11-03 14:50:11 +0100

    added Docstrings to RecordAdapter
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- rewrite/lib/utils/db/RecordAdapter.py
+++ rewrite/lib/utils/db/RecordAdapter.py
@@ -10,12 +10,32 @@ CHOICES = ('CONTROL', 'DATA', 'TEMPERATURE', 'PRESSURE', 'COUNTER')
 
 
 class RecordAdapter(Document):
+    """
+    This is an adapter class which helps to save and load a record in MongoDB.
+
+
+    :param _id: Object ID given by MongoDB. Explicitly declared to be able to load from dict.
+    :param packageNumber: A sequential number of all packages send by a DAQ server
+    :param RecType: Type of the record
+    :param timestamp: Unixtimestamp
+    :param payload_cnt: Count Payload
+    :param payload_dat: Data Payload
+    :param payload_tme: Temperature Payload
+    :param payload_prs: Pressure Payload
+
+    Sadly the payload for each type of payload needs to be in a separate field, as we need an EmbeddedDocumentField of a certain type.
+
+
+    """
     meta = {
         'collection': 'records'
     }
     _id = ObjectIdField()
     packageNumber = IntField()
     type = StringField(choices=CHOICES)
+    """
+    :String: only valid choices here are: CONTROL, DATA, TEMPERATURE, PRESSURE, COUNTER
+    """
     timestamp = DateTimeField()
     payload_cnt = EmbeddedDocumentField(CountRecordAdapter)
     payload_dat = EmbeddedDocumentField(DataRecordAdapter)
@@ -23,17 +43,31 @@ class RecordAdapter(Document):
     payload_prs = EmbeddedDocumentField(PressureRecordAdapter)
 
     def __init__(self, **kwargs):
+        """
+        Call the superclass constructor and provide functionality to construct from a dict.
+        """
         super(RecordAdapter, self).__init__(**kwargs)
         self.__dict__.update(kwargs)
 
     @staticmethod
     def getChoice(i):
+        """
+        Translate RecordType aka int to string.
+
+        :param i: RecordType to be converted.
+        """
         for v in CHOICES:
             if v[0] == i:
                 return v[1]
 
     @staticmethod
     def get(rec):
+        """
+        Construct a RecordAdapter from a Record
+
+        :param rec: a record that will be converted to a RecordAdapter
+        :returns: a newly constructed  RecordAdapter
+        """
         if rec.type == RecordType.COUNTER:
             return RecordAdapter(packageNumber=rec.packageNumber, type=CHOICES[int(rec.type)], timestamp=datetime.fromtimestamp(
                 rec.timestamp), payload_cnt=CountRecordAdapter.get(rec.payload))
@@ -47,7 +81,12 @@ class RecordAdapter(Document):
             return RecordAdapter(packageNumber=rec.packageNumber, type=CHOICES[int(rec.type)], timestamp=datetime.fromtimestamp(
                 rec.timestamp), payload_prs=PressureRecordAdapter.get(rec.payload))
 
-    def set(self):
+    def createRecord(self):
+        """
+        Creates a Record with the current data.
+
+        :return: Record with the current data
+        """
         recPayload = None
         recType = None
         if self.type == 'CONTROL':

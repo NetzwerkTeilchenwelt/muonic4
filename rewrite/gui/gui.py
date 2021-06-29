af81017959de6b5eeb100edef5d7e4cdd43c93e5
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2021-06-29 11:36:15 +0200

    moved guitest.py to gui
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- rewrite/gui/gui.py
+++ rewrite/gui/gui.py
@@ -5,67 +5,75 @@ import sys
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('gui/muonic4.ui', self)
+        uic.loadUi("gui/muonic4.ui", self)
 
         # Get buttons for open studies
         self.btOpenStudiesStop = self.findChild(
-            QtWidgets.QPushButton, 'btOpenStudiesStop')
+            QtWidgets.QPushButton, "btOpenStudiesStop"
+        )
         self.btOpenStudiesStart = self.findChild(
-            QtWidgets.QPushButton, 'btOpenStudiesStart')
+            QtWidgets.QPushButton, "btOpenStudiesStart"
+        )
 
         self.btOpenStudiesStart.clicked.connect(self.btOpenStudiesStartClicked)
         self.btOpenStudiesStop.clicked.connect(self.btOpenStudiesStopClicked)
 
         # Get values for open studies
         self.ckOpenStudiesCH0Enabled = self.findChild(
-            QtWidgets.QCheckBox, 'ckOpenStudiesCH0Enabled')
+            QtWidgets.QCheckBox, "ckOpenStudiesCH0Enabled"
+        )
         self.OpenStudiesCH0Voltage = self.findChild(
-            QtWidgets.QSpinBox, 'OpenStudiesCH0Voltage')
+            QtWidgets.QSpinBox, "OpenStudiesCH0Voltage"
+        )
         self.ckOpenStudiesCH1Enabled = self.findChild(
-            QtWidgets.QCheckBox, 'ckOpenStudiesCH1Enabled')
+            QtWidgets.QCheckBox, "ckOpenStudiesCH1Enabled"
+        )
         self.OpenStudiesCH1Voltage = self.findChild(
-            QtWidgets.QSpinBox, 'OpenStudiesCH1Voltage')
+            QtWidgets.QSpinBox, "OpenStudiesCH1Voltage"
+        )
         self.ckOpenStudiesCH2Enabled = self.findChild(
-            QtWidgets.QCheckBox, 'ckOpenStudiesCH2Enabled')
+            QtWidgets.QCheckBox, "ckOpenStudiesCH2Enabled"
+        )
         self.OpenStudiesCH2Voltage = self.findChild(
-            QtWidgets.QSpinBox, 'OpenStudiesCH2Voltage')
+            QtWidgets.QSpinBox, "OpenStudiesCH2Voltage"
+        )
         self.ckOpenStudiesCH3Enabled = self.findChild(
-            QtWidgets.QCheckBox, 'ckOpenStudiesCH3Enabled')
+            QtWidgets.QCheckBox, "ckOpenStudiesCH3Enabled"
+        )
         self.OpenStudiesCH3Voltage = self.findChild(
-            QtWidgets.QSpinBox, 'OpenStudiesCH3Voltage')
+            QtWidgets.QSpinBox, "OpenStudiesCH3Voltage"
+        )
 
         # Get radio buttons for open studies coincidence
         self.OpenStudiesSingleCoincidence = self.findChild(
-            QtWidgets.QRadioButton, 'OpenStudiesSingleCoincidence')
+            QtWidgets.QRadioButton, "OpenStudiesSingleCoincidence"
+        )
         self.OpenStudiesTwoFoldCoincidence = self.findChild(
-            QtWidgets.QRadioButton, 'OpenStudiesTwoFoldCoincidence')
+            QtWidgets.QRadioButton, "OpenStudiesTwoFoldCoincidence"
+        )
         self.OpenStudiesThreeFoldCoincidence = self.findChild(
-            QtWidgets.QRadioButton, 'OpenStudiesThreeFoldCoincidence')
+            QtWidgets.QRadioButton, "OpenStudiesThreeFoldCoincidence"
+        )
         self.OpenStudiesFourFoldCoincidence = self.findChild(
-            QtWidgets.QRadioButton, 'OpenStudiesFourFoldCoincidence')
+            QtWidgets.QRadioButton, "OpenStudiesFourFoldCoincidence"
+        )
 
         # Get trigger window for open studies
         self.OpenStudiesTriggerWindow = self.findChild(
-            QtWidgets.QSpinBox, 'OpenStudiesTriggerWindow')
+            QtWidgets.QSpinBox, "OpenStudiesTriggerWindow"
+        )
 
         self.show()
 
     def btOpenStudiesStartClicked(self):
         print(
-            f"Btn clicked {self.ckOpenStudiesCH0Enabled.isChecked()} -> {self.OpenStudiesCH0Voltage.text()}")
+            f"Btn clicked {self.ckOpenStudiesCH0Enabled.isChecked()} -> {self.OpenStudiesCH0Voltage.text()}"
+        )
 
     def btOpenStudiesStopClicked(self):
         print("Stop")
 
+
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
-# app = QApplication([])
-# window = QWidget()
-# layout = QVBoxLayout()
-# layout.addWidget(QPushButton('Top'))
-# layout.addWidget(QPushButton('Bottom'))
-# window.setLayout(layout)
-# window.show()
-
-# app.exec()

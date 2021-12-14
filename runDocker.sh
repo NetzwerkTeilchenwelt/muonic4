21f8a3a4d1dfc8cef0326d8f6da975ba11826c8c
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2021-12-14 12:44:45 +0100

    added docker-compose.yml
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- runDocker.sh
+++ runDocker.sh
@@ -36,25 +36,28 @@ wutzi/muonic python3 /app/rewrite/runGui.py
       export IP=`ipconfig getifaddr en0`
     fi
     export DISPLAY=$IP:0
-    export PATH=/usr/X11/bin/xhost:$PATH
+    # if ! command -v xhost &>/dev/null then
+    #   export PATH=/usr/X11/bin/xhost:$PATH
+    # fi
     xhost + $IP
-    docker run --rm -it \
-   --user=$(id -u) \
-   --env="DISPLAY" \
-   -e DISPLAY=$DISPLAY \
-   -v /dev:/dev \
-   --workdir=/app \
-   --volume="$PWD":/app \
-   --volume="/etc/group:/etc/group:ro" \
-   --volume="/etc/passwd:/etc/passwd:ro" \
-   --volume="/etc/shadow:/etc/shadow:ro" \
-   --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-   -v /dev:/dev \
-   -v /var:/var \
-   --privileged \
-   -it \
-wutzi/muonic python3 /app/rewrite/runGui.py
+    docker-compose up
+#     docker run --rm -it \
+#    --user=$(id -u) \
+#    --env="DISPLAY" \
+#    -e DISPLAY=$DISPLAY \
+#    -v /dev:/dev \
+#    --workdir=/app \
+#    --volume="$PWD":/app \
+#    --volume="/etc/group:/etc/group:ro" \
+#    --volume="/etc/passwd:/etc/passwd:ro" \
+#    --volume="/etc/shadow:/etc/shadow:ro" \
+#    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+#    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#    -v /dev:/dev \
+#    -v /var:/var \
+#    --privileged \
+#    -it \
+# wutzi/muonic python3 /app/rewrite/runGui.py
     ;;
   'SunOS')
     OS='Solaris'
